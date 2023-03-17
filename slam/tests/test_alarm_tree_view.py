fa70b06f5c180825e096385e4a00550235469b57
jbellister-slac <jesseb@slac.stanford.edu>
2023-03-16 17:27:01 -0700

    TST: Update tests to work with code cleanup

--- slam/tests/test_alarm_tree_view.py
+++ slam/tests/test_alarm_tree_view.py
@@ -45,11 +45,15 @@ def test_tree_menu(qtbot, monkeypatch, alarm_tree_view):
         alarm_tree_view.context_menu.show()
 
 
-def test_send_acknowledgement(qtbot, monkeypatch, alarm_tree_view, mock_kafka_producer):
-    """ Test that when a user acknowledges an alarm from the tree, the message sent to kafka is correct """
+@pytest.mark.parametrize('acknowledged', [False, True])
+def test_acknowledge_action(qtbot, monkeypatch, alarm_tree_view, mock_kafka_producer, acknowledged):
+    """ Test that when a user acks or un-acks an alarm from the tree, the message sent to kafka is correct """
     qtbot.addWidget(alarm_tree_view)
-    # Create an alarm with a severity of major
-    alarm_item = AlarmItem('TEST:PV', path='/path/to/TEST:PV', alarm_severity=AlarmSeverity.MAJOR)
+    # Create an alarm with an acknowledged state opposite to what we want to set
+    if acknowledged:
+        alarm_item = AlarmItem('TEST:PV', path='/path/to/TEST:PV', alarm_severity=AlarmSeverity.MAJOR)
+    else:
+        alarm_item = AlarmItem('TEST:PV', path='/path/to/TEST:PV', alarm_severity=AlarmSeverity.MAJOR_ACK)
 
     # Monkeypatch some qt methods to return our alarm as the selected index
     model_index = QModelIndex()
@@ -58,67 +62,32 @@ def test_send_acknowledgement(qtbot, monkeypatch, alarm_tree_view, mock_kafka_pr
     monkeypatch.setattr(alarm_tree_view.treeModel, 'getItem', lambda x: alarm_item)
     alarm_tree_view.treeModel.added_paths['TEST:PV'] = ['/path/to/TEST:PV']
 
-    alarm_tree_view.send_acknowledgement()
+    alarm_tree_view.send_action(acknowledged=acknowledged)
     # Setting the correct topic, path, and acknowledgement command is all we need to acknowledge an alarm
     assert mock_kafka_producer.topic == 'TEST_TOPICCommand'
     assert mock_kafka_producer.key == 'command:/path/to/TEST:PV'
     assert 'command' in mock_kafka_producer.values
-    assert mock_kafka_producer.values['command'] == 'acknowledge'
+    if acknowledged:
+        assert mock_kafka_producer.values['command'] == 'acknowledge'
+    else:
+        assert mock_kafka_producer.values['command'] == 'unacknowledge'
 
 
-def test_send_unacknowledgement(qtbot, monkeypatch, alarm_tree_view, mock_kafka_producer):
-    """ Test than when a user unacknowledges an alarm from the tree, the message sent to kafka is correct """
-    qtbot.addWidget(alarm_tree_view)
-    # Create an alarm with a severity of major ack
-    alarm_item = AlarmItem('TEST:PV', path='/path/to/TEST:PV', alarm_severity=AlarmSeverity.MAJOR_ACK)
-
-    # Monkeypatch some qt methods to return our alarm as the selected index
-    model_index = QModelIndex()
-    indices = [model_index]
-    monkeypatch.setattr(alarm_tree_view.tree_view, 'selectedIndexes', lambda: indices)
-    monkeypatch.setattr(alarm_tree_view.treeModel, 'getItem', lambda x: alarm_item)
-    alarm_tree_view.treeModel.added_paths['TEST:PV'] = ['/path/to/TEST:PV']
-
-    alarm_tree_view.send_unacknowledgement()
-    # Setting the correct topic, path, and acknowledgement command is all we need to acknowledge an alarm
-    assert mock_kafka_producer.topic == 'TEST_TOPICCommand'
-    assert mock_kafka_producer.key == 'command:/path/to/TEST:PV'
-    assert 'command' in mock_kafka_producer.values
-    assert mock_kafka_producer.values['command'] == 'unacknowledge'
-
-
-def test_enable_alarm(qtbot, monkeypatch, alarm_item, alarm_tree_view, mock_kafka_producer):
-    """ Test that when a user enables an alarm from the tree, the message sent to kafka is correct """
+@pytest.mark.parametrize('enabled', [False, True])
+def test_enable_disable_action(qtbot, monkeypatch, alarm_item, alarm_tree_view, mock_kafka_producer, enabled):
+    """ Test that when a user enables or disables an alarm from the tree, the message sent to kafka is correct """
     qtbot.addWidget(alarm_tree_view)
     model_index = QModelIndex()
     indices = [model_index]
     alarm_item.description = 'Test Alarm'
-    alarm_item.enabled = False  # Disable the alarm so we can enable it later
+    alarm_item.enabled = not enabled  # Set it to the opposite of the action we want to take
     monkeypatch.setattr(alarm_tree_view.tree_view, 'selectedIndexes', lambda: indices)
     monkeypatch.setattr(alarm_tree_view.treeModel, 'getItem', lambda x: alarm_item)
     alarm_tree_view.treeModel.added_paths['TEST:PV:ONE'] = ['/ROOT/SECTOR_ONE/TEST:PV:ONE']
 
-    alarm_tree_view.enable_alarm()
+    alarm_tree_view.send_action(enabled=enabled)
     assert mock_kafka_producer.topic == 'TEST_TOPIC'
     assert mock_kafka_producer.key == 'config:/ROOT/SECTOR_ONE/TEST:PV:ONE'
     assert mock_kafka_producer.values['description'] == 'Test Alarm'
-    assert mock_kafka_producer.values['enabled']
-    assert not mock_kafka_producer.values['latching']
-
-
-def test_disable_alarm(qtbot, monkeypatch, alarm_item, alarm_tree_view, mock_kafka_producer):
-    """ Test that when a user disables an alarm from the tree, the message sent to kafka is correct """
-    qtbot.addWidget(alarm_tree_view)
-    model_index = QModelIndex()
-    indices = [model_index]
-    alarm_item.description = 'Test Alarm'
-    monkeypatch.setattr(alarm_tree_view.tree_view, 'selectedIndexes', lambda: indices)
-    monkeypatch.setattr(alarm_tree_view.treeModel, 'getItem', lambda x: alarm_item)
-    alarm_tree_view.treeModel.added_paths['TEST:PV:ONE'] = ['/ROOT/SECTOR_ONE/TEST:PV:ONE']
-
-    alarm_tree_view.disable_alarm()
-    assert mock_kafka_producer.topic == 'TEST_TOPIC'
-    assert mock_kafka_producer.key == 'config:/ROOT/SECTOR_ONE/TEST:PV:ONE'
-    assert mock_kafka_producer.values['description'] == 'Test Alarm'
-    assert not mock_kafka_producer.values['enabled']
+    assert mock_kafka_producer.values['enabled'] == enabled
     assert not mock_kafka_producer.values['latching']
